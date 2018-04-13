23e65223ff9c5f9110ec712f2f77cfb29c7903d4
Ricardo Silveira <ricardofsilveiralbj@gmail.com>
2018-04-13 20:08:28 +0100

    Added Facebook Login.

--- public/app.js
+++ public/app.js
@@ -1,20 +1,49 @@
 document.addEventListener("DOMContentLoaded", event => {
 
-    const app = firebase.app();
+    var app = firebase.app();
 
 });
 
 function googleLogin(){
-    const provider = new firebase.auth.GoogleAuthProvider();
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().useDeviceLanguage();
 
     firebase.auth().signInWithPopup(provider)
 
             .then(result => {
-                const user = result.user;
+                var user = result.user;
                 document.write(`Hello ${user.displayName}`);
                 console.log(user);
             })
             .catch(console.log);
+}
+
+function facebookLogin(){
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().useDeviceLanguage();
+
+    firebase.auth().signInWithPopup(provider)
+
+            .then(function(result) {
+                // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+
+                // document.write(`Hello ${user}`);
+                console.log(user);
+                // ...
+            }).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+      });
 }
 
 
