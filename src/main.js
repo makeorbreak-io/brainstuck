c1d30a0a2a65944cba018325a946556cbe01dd42
Ricardo Silveira <ricardofsilveiralbj@gmail.com>
2018-04-13 23:22:09 +0100

    Restructured project to Vue.js framework

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import { store } from './store'
+import firebase from 'firebase'
+
+Vue.use(Vuetify)
+firebase.initializeApp({
+  apiKey: 'AIzaSyC2ht4lndm4Fh175jILIwPg3pprzw7i7-g',
+  authDomain: 'brainstuck-65ca2.firebaseapp.com',
+  databaseURL: 'https://brainstuck-65ca2.firebaseio.com',
+  projectId: 'brainstuck-65ca2'
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+const unsubscribe = firebase.auth()
+.onAuthStateChanged((firebaseUser) => {
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App),
+    created () {
+      if (firebaseUser) {
+        store.dispatch('autoSignIn', firebaseUser)
+      }
+    }
+  })
+  unsubscribe()
+})
